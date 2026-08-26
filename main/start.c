fca185856085616f0d69b1fed2f0c0b6e5706859
yuanlecai029-cyber <yuanlecai029@gmail.com>
2026-08-26 09:03:16 +0800

    LCD+follow

--- main/start.c
+++ main/start.c
@@ -1,18 +1,19 @@
 #include "start.h"
 #include "motor.h"
+#include "lcd.h"
 #include "follow_brain.h"
 #include "pins.h"
 #include "driver/gpio.h"
 
 // ================================================================
-// 启动函数：启动循迹程序
-//     
+// 启动函数：启动循迹程序    
 // ================================================================
 
 void start(void)
 {    
     motor_init();
     ir_init();   // 先初始化，follow() 才能读到电平
+    lcd_init();  // 液晶屏幕初始化  
 
     double speed = 0.15;
     motor_forward(speed);
@@ -20,6 +21,11 @@ void start(void)
     while (turns++ < 100)
     {
         follow();
+        //把四路电平IR1~IR4从左往右依次显示
+        lcd_show_ir((uint8_t)gpio_get_level(IR1),
+                    (uint8_t)gpio_get_level(IR2),
+                    (uint8_t)gpio_get_level(IR3),
+                    (uint8_t)gpio_get_level(IR4));
     }
     motor_stop();
 }
